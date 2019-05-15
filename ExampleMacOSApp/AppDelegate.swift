a3bc36076e15689111e383b59f3c15461cb376ee
Franco Rivera <franco@riverarivas.com>
2019-05-15 15:11:47 -0500

    Initial Commit

---
+++ ExampleMacOSApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ExampleMacOSApp
+//
+//  Created by Franco Rivera on 5/15/19.
+//  Copyright © 2019 WeMake. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ExampleMacOSApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ExampleMacOSApp
+//
+//  Created by Franco Rivera on 5/15/19.
+//  Copyright © 2019 WeMake. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
